7db0dfdb9e409ca8d4f88b3b58f38c6732b50c57
Hilary-Collado <hilarycollado1@gmail.com>
2024-12-03 10:14:39 -0400

    Agregando formulario "registro de compania", colocando campos

---
+++ src/Components/Formularios/Empresa/RegistroCompanhia.jsx
@@ -0,0 +1,65 @@
+
+const RegistroCompania = () => {
+    return (
+        <>
+
+            <h3 class="card-title">Ingreso de Compañia</h3>
+
+            {/* <!-- General Form Elements --> */}
+            <form class="needs-validation" id="form" novalidate>
+
+                <div class="row mb-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="idCompañia" class="form-label">ID Compañia</label>
+                        <input type="text" class="form-control" id="idCompañia" disabled />
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" required />
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="rnc" class="form-label">RNC</label>
+                            <input type="text" class="form-control" id="rnc" disabled />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="tipoEmpresa" class="form-label">Tipo empresa</label>
+                        <input type="text" class="form-control" id="tipoEmpresa"/>
+                    </div>
+                </div>
+
+                <div class="row mb-5">
+                    <div class="col-md-6 mb-3">
+                        <label for="status" class="form-label">Status</label>
+                        <select id="status" class="form-select" required>
+                            <option value="" disabled selected>Selecciona Una Opcion...</option>
+                            <option value="1">Activo</option>
+                            <option value="0">Inactivo</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="fechaModificacion" class="form-label">Fecha de Modificacion</label>
+                        <input type="date" id="fechaModificacion" class="form-control" disabled/>
+                    </div>
+                </div>
+
+                <div class="text-center mb-3">
+                    <button type="submit" class="btn btn-primary buttonSubmit">Enviar</button>
+                    <button type="reset" class="btn btn-outline-danger">Limpiar</button>
+                </div>
+
+            </form>
+            {/* <!-- End General Form Elements --> */}
+
+
+        </>
+    )
+}
