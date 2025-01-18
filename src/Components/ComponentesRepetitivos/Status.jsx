99bb1817abe152a0391f9c36b097016f9b07be87
Hilary-Collado <hilarycollado1@gmail.com>
2025-01-18 17:58:29 -0400

    colocando la opcion "activo" en el campo status por default

--- src/Components/ComponentesRepetitivos/Status.jsx
+++ src/Components/ComponentesRepetitivos/Status.jsx
@@ -4,10 +4,9 @@ const Status = () => {
             <div className="col-md-6 mb-3">
                 <label for="status" className="form-label">Status</label>
                 <select id="status" className="form-select" required>
-                    <option value="" disabled selected>Selecciona Una Opcion...</option>
-                    <option value="1">Activo</option>
+                    <option value="1" selected>Activo</option>
                     <option value="0">Inactivo</option>
-                </select>
+                </select> 
             </div>
         </>
     )
